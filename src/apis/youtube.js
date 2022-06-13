bba9c7c05985bb64e187dbb578f049d0abce8fda
Mertakbas <mertakbas@saatteknoloji.com>
2022-06-13 18:41:27 +0300

    searching for videos

---
+++ src/apis/youtube.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyAJWCayzdZq2uNfL9ZtPIHiv_RmRH08Xhw';
+
+export default axios.create ({
+   baseURL:'https://www.googleapis.com/youtube/v3',
+   params: {
+    part:'snippet',
+    maxResults: 5,
+    key: KEY
+   }
+});
+
+
