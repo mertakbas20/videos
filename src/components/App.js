38c11a308f047ccbaceea39cab0ce624e23c1168
Mertakbas <mertakbas@saatteknoloji.com>
2022-06-14 15:12:28 +0300

    deeply nested callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: {
@@ -14,11 +14,15 @@ class App extends React.Component {
         this.setState({ videos: response.data.items });
     };
 
+    onVideoSelect = (video) => {
+        console.log('From the App!', video);
+    };
+
     render() {
         return (
             <div className='ui container'>
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
     }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,14 +1,14 @@
 import './VideoItem.css';
 import React from "react";
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
     return (
-        <div className="video-item item">
+        <div onClick={() => onVideoSelect(video)} className="video-item item">
             <img className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content">
-                <a className="header">
+                <div className="header">
                     {video.snippet.title}
-                </a>
+                </div>
             </div>
 
         </div>
