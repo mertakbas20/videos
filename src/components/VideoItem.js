43b844b4eca0ea80dab266f59a6ea531b11cdb39
Mertakbas <mertakbas@saatteknoloji.com>
2022-06-14 13:58:12 +0300

    styling a list

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,12 +1,18 @@
+import './VideoItem.css';
 import React from "react";
 
 const VideoItem = ({ video }) => {
     return (
-        <div>
-            <img src={video.snippet.thumbnails.medium.url} />
-            {video.snippet.title}
+        <div className="video-item item">
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+            <div className="content">
+                <a className="header">
+                    {video.snippet.title}
+                </a>
+            </div>
+
         </div>
-    ); 
+    );
 };
 
 export default VideoItem;
